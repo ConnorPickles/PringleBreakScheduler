73f359afac8049e5460c2e47e20cd6da590cb830
Connor Pickles <cpickles98@gmail.com>
2017-09-18 01:19:23 -0700

    New Information Page Started
    some inital navigation added

--- script.js
+++ script.js
@@ -4,17 +4,28 @@
 * Date:    September 11th, 2017
 * Purpose: To create break schedules for Camp Pringle's summer program
 *************************************************************************/
-var currentScreen = "home";
+var currentMain = "home"; // current main page
+var currentNewInfo = "newLeader"; // current newInfo page
 
 function init() {
 
 }
 
 // makes the displayed screen newScreen
-function changeScreen(newScreen) {
+function changeScreen(page, newScreen) {
+    var currentScreen = "";
+    switch (page) {
+        case "main":
+            currentScreen = currentMain;
+            currentMain = newScreen;
+            break;
+        case "newInfo":
+            currentScreen = currentNewInfo;
+            currentNewInfo = newScreen;
+            break;
+    } // switch
     document.getElementById(currentScreen).className = "hidden";
     document.getElementById(newScreen).className = "visible";
-    currentScreen = newScreen;
 } // changeScreen
 
 // switches the visibility of the given element
